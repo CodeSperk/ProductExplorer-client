7f2a0d2afd746725c413b78512478ba9dbc8717c
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-08-16 20:44:32 +0600

    styled product card

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -22,11 +22,11 @@ const Products = () => {
 
 
      {/* Products */}
-      <div>
+      <div className="">
         {isLoading ? ( 
           <div>Loading </div> 
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center md:justify-between gap-y-16 gap-x-4">
             {products.map((product) => <ProductCard key={product._id} product={product} />)}
           </div>
         )}
