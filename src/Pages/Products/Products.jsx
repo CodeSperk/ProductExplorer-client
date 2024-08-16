faff9391f213177b13c62b6ace6cb3e3d4587218
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-08-16 19:09:50 +0600

    loaded product data using tanstack query and axios

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,7 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import ProductCard from "./ProductCard";
+
 const Products = () => {
+  const [filterQuery, setFilterQuery] = useState("");
+
+  const {isLoading, data: products = [], refetch} = useQuery({
+    queryKey: ["allProducts", filterQuery],
+    queryFn: async () => {
+      const res = await axios.get(`http://localhost:5000/products`);
+      console.log(res.data);
+      return res.data;
+    }
+  })
+
+
   return (
     <div>
-      Products
+      
+
+
+     {/* Products */}
+      <div>
+        {isLoading ? ( 
+          <div>Loading </div> 
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {products.map((product) => <ProductCard key={product._id} product={product} />)}
+          </div>
+        )}
+      </div>
+
+
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Home from './Pages/Home/Home'
-import Products from './Pages/Products/Products'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <App />,
-    children:[
+    children: [
       {
-        path:"/",
-        element: <Home/>
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/products",
-        element: <Products/>
-      }
-    ]
-  }
-])
+        path: "/products",
+        element: <Products />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
